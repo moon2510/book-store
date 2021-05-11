d0884e1f372b44dc297a55ccac870786ba21313f
moon2510 <62028865+moon2510@users.noreply.github.com>
2021-05-12 04:40:03 +0700

    v1.1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
       type="text/css" />
    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
       integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-   <title>Book Store/title>
+   <title>Book Store</title>
 </head>
 <header>
    <div class="first">
