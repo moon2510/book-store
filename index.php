6c81afe2629c6eb54a4607006f8c0b73c6e38b31
moon2510 <62028865+moon2510@users.noreply.github.com>
2021-05-12 04:20:33 +0700

    v1

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="/CSS/layout.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+   <link href="//db.onlinewebfonts.com/c/cefd416d4a810c38e8e2987afa6ef07f?family=Grendel's+Mother" rel="stylesheet"
+      type="text/css" />
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+   <title>Book Store/title>
+</head>
+<header>
+   <div class="first">
+      <div class="left">
+         <div class="logo">
+            <i class="fas fa-book-dead"></i>
+         </div>
+      </div>
+      <div class="center">
+         <div class="navbar">
+            <button class="snip">HOME</button>
+            <button class="snip">BOOKS</button>
+            <button class="snip">NEW</button>
+            <button class="snip">SALE</button>
+            <button class="snip">SUBSCRIBE</button>
+            <button class="snip">CONTACT</button>
+         </div>
+         
+      </div>
+      <div class="right">
+         <div class="search">
+            <button class="fas fa-search"></button>
+         </div>
+         <div class="menu"></div>
+      </div>   
+     
+   </div>
+</header>
+<body>
+   
+   <div class="container">
+      <div class="left lefttwo">
+         <div></div>
+         <div>
+         <li class="white"><i class="far fa-circle"></i></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         </div>
+         <div></div>
+         
+      </div>
+      <div class="center">
+         <div class="number">
+            <h1>02</h1>
+         </div>
+         <div class="describe">
+            <h2>Dedicated bookstore for serious readers</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+               Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur.</h3>
+         </div>
+         <div class="shopnow">
+            <button class="shop">SHOP NOW<i class="fas fa-long-arrow-alt-right arrow"></i></button>
+            
+         </div>
+         <div class="books">
+               <div class="book">
+                  <img src="/Books/9200000031352565.jpg" alt="">
+                  <h4>The $100 startup <br><i>Chris Guillabeau</i></br></h4>
+               </div>
+               <div class="book">
+                  <img src="/Books/9200000074408713.jpg" alt="">
+                  <h4>Ego is the enemy <br><i>Ryan Holiday</i></br> </h4>
+               </div>
+               <div class="book">
+                  <img src="/Books/1001004000772645.jpg" alt="">
+                  <h4>The power of now <br><i>Eckhart Tolle</i></br></h4>
+               </div>
+               <div class="book">
+                  <img src="/Books/9200000075590380.jpg" alt="">
+                  <h4>Building a brand story <br><i>Donald Miller</i></br></h4>
+               </div>
+         </div>
+      </div>
+      <div class="social">
+         <div class="social-icon">
+            <button></button>
+            <button></button>
+            <button class="fab fa-instagram"></button>
+            <button class="fab fa-youtube-square"></button>
+            <button class="fab fa-facebook-messenger"></button>
+            <button class="fab fa-twitter"></button>
+            <button class="fab fa-snapchat-ghost"></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+         </div>
+      </div>
+   </div>
+</body>
+<!-- <footer>
+   <div class="books">
+      <div></div>
+      <div class="center">
+         <img src="/Books/1001004000772645.jpg" alt="">
+      </div>
+      <div></div>
+   </div>
+</footer> -->
+</html>
